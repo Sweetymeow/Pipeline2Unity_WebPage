5a2948e3a912cb004a3751d72ac0ba582e0c5596
WendySCDC <honeykatie@qq.com>
2015-03-14 02:46:34 -0400

    send message from html to Unity / Instructors GUI x2
    Delete the $firephp part in acceptData.php

--- acceptData.php
+++ acceptData.php
@@ -1,22 +1,13 @@
 <?php
 
-// Using PHP Console in Chrome to debug
-require_once('FirePHPCore/FirePHP.class.php');
-ob_start();
-$firephp = FirePHP::getInstance(true); 
-$firephp->warn('FirePhp Work!'); 
-// FirePhp debug
-
 $flashKey = isset($_GET['key1']);
 
 if ($flashKey) { 
 	// Get Data from Flash
-	$firephp->warn('Get Flash key1'); // FirePhp debug
 	$saveKey = $_GET['key1'];
 	$myfile = fopen("pipename.txt", "w") or die("Unable to open file!");
 	fwrite($myfile, $saveKey);
 	$myfile = fopen("pipename.txt", "r") or die("Unable to open file!");
-	$firephp->log(array("Get key1 : " => $saveKey), 'flashNum'); // FirePhp debug
 	echo "Accept key1 data: ";
 	echo fgets($myfile);
 	echo "\n";
